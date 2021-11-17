cf8f7e0e6b42c9ac270fd83e4496df99df4ac62f
Jonas Nolde <jonasnoldedev@gmail.com>
2021-11-17 09:41:18 +0100

    Create learn app

---
+++ codi-lernapp/src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header role="banner">
+    <nav>
+      <ul>
+        <li>
+            <v-link href="/">Home</v-link>
+        </li>
+        <li>
+            <v-link href="/about">About</v-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+import VLink from "../components/VLink.vue";
+
+export default {
+  name: 'AppHeader',
+  components: {
+    VLink,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  padding: 1rem;
+  background-color: navajowhite;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  margin-right: .5rem;
+}
+</style>
---
+++ codi-lernapp/src/layouts/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-wrapper">
+    <app-header></app-header>
+    <main class="main" role="main">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<style scoped>
+.main {
+    padding: 1rem;
+}
+</style>

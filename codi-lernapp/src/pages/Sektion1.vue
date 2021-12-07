a9a164716846577f385ff06720d42765116129be
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2021-12-07 20:06:18 +0100

    Backarrow functional

--- codi-lernapp/src/pages/Sektion1.vue
+++ codi-lernapp/src/pages/Sektion1.vue
@@ -1,7 +1,7 @@
 <template>
   <main-layout>
       <header>
-        <v-button href="/"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></v-button>
+        <v-button href="/lernsektionen"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></v-button>
       </header>
     <Carousal class="carousal" v-slot="{currentSlide}">
       <Slide>
@@ -14,12 +14,14 @@
         <div v-show="currentSlide === 2" class="slide-info">
           <img :src="require(`../assets/img/video.png`)">
           <p>slider 2</p>
+          <div class="left-point"></div>
         </div>
       </Slide>
       <Slide>
         <div v-show="currentSlide === 3" class="slide-info">
           <img :src="require(`../assets/img/Gif.png`)">
           <p>slider 3</p>
+          <div class="left-point"></div>
         </div>
       </Slide>
     </Carousal>

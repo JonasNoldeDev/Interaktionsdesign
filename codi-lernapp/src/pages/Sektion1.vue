17ed4042e40525a39aff056933d85f464b4b0320
johnxdvpj <37866793+johnxdvpj@users.noreply.github.com>
2021-12-07 20:02:52 +0100

    Slider Speech Bubble Update Backarrow

--- codi-lernapp/src/pages/Sektion1.vue
+++ codi-lernapp/src/pages/Sektion1.vue
@@ -1,5 +1,8 @@
 <template>
   <main-layout>
+      <header>
+        <v-button href="/"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></v-button>
+      </header>
     <Carousal class="carousal" v-slot="{currentSlide}">
       <Slide>
         <div v-show="currentSlide === 1" class="slide-info speech-bubble">
@@ -29,13 +32,15 @@
 
   import Carousal from '../components/Carousal.vue'
   import Slide from '../components/Slide.vue'
+  import VButton from '../components/VButton.vue'
 
 
   export default {
     components: {
       MainLayout,
       Carousal,
-      Slide
+      Slide,
+      VButton
     },
     setup() {
       const carousalSlides = ['img', 'Gif', 'video']
@@ -46,13 +51,24 @@
 </script>
 
 <style lang="scss" scoped>
+  header {
+    display: flex;
+    align-items: center;
+    margin: 1rem 0 2rem;
+
+    svg {
+      display: block;
+      width: 1em;
+      height: 1em;
+    }
+  }
   .carousal{
     position: relative;
     max-height: 100%;
-    height: 90%;
+    height: 88%;
     .slide-info{
       position: absolute;
-      top: 25%;
+      top: 5%;
       left: 25%;
       width: 50%;
       max-height: 100%;

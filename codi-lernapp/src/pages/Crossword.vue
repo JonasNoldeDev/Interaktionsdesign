81ce52ab87f45d3452afb243da3aea5b49b8bd3f
Leonie <leonie.kahn@htwg-konstanz.de>
2022-01-05 22:02:31 +0100

    completed crossword inputs

--- codi-lernapp/src/pages/Crossword.vue
+++ codi-lernapp/src/pages/Crossword.vue
@@ -15,7 +15,7 @@
         <img class="bubble" v-if="showBubble" alt="Super"
              src="./../assets/img/characters/codi/Codi_richtig_Sprechblase.png">
 
-        <div class="row_7 position2">
+        <div class="row_7 position1">
             <input id="a1" class="size" alt="Eingabefeld" type="text" maxlength="1"
                    @inpu="$emit('userinput')">
             <input id="a2" class="size" alt="Eingabefeld" type="text" maxlength="1"
@@ -32,7 +32,7 @@
                    @input="$emit('userinput')">
         </div>
 
-        <div class="col_12 position3">
+        <div class="col_12 position2">
             <div id="5" class="position4" style="margin-top: 1rem">
                 <input id="b2" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
@@ -45,10 +45,17 @@
                 <input id="b4" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
             </div>
-            <div class="position4">
-                <input id="b5" class="size" alt="Eingabefeld" type="text" maxlength="1"
+
+            <div class="row_3 position3">
+
+                <input id="c1" class="size" alt="Eingabefeld" type="text" maxlength="1"
+                       @input="$emit('userinput')">
+                <input id="b5/c2" class="size" alt="Eingabefeld" type="text" maxlength="1"
+                       @input="$emit('userinput')">
+                <input id="c3" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
             </div>
+
             <div class="position4">
                 <input id="b6" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
@@ -65,8 +72,14 @@
                 <input id="b9" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
             </div>
-            <div class="position4">
-                <input id="b10" class="size" alt="Eingabefeld" type="text" maxlength="1"
+            <div class="row_4 position3">
+                <input id="d1" class="size" alt="Eingabefeld" type="text" maxlength="1"
+                       @input="$emit('userinput')">
+                <input id="d2/b10" class="size" alt="Eingabefeld" type="text" maxlength="1"
+                       @input="$emit('userinput')">
+                <input id="d3" class="size" alt="Eingabefeld" type="text" maxlength="1"
+                       @input="$emit('userinput')">
+                <input id="d4" class="size" alt="Eingabefeld" type="text" maxlength="1"
                        @input="$emit('userinput')">
             </div>
             <div class="position4">
@@ -78,12 +91,9 @@
                        @input="$emit('userinput')">
             </div>
         </div>
-        <!--
-            <Row v-on:userinput="submit"></Row>
-            <Column style="margin-left: 28rem"></Column>
-            -->
     </main-layout>
 </template>
+
 
 <!--
 <template>
@@ -266,6 +276,20 @@
         margin: 0.5rem;
     }
 
+    .row_3 {
+        background-image: url(../assets/img/Holz_mitSchatten_3.png);
+        background-repeat: no-repeat;
+        background-size: 35%;
+        padding: 1.5rem;
+    }
+
+    .row_4 {
+        background-image: url(../assets/img/Holz_mitSchatten_4.png);
+        background-repeat: no-repeat;
+        background-size: 47%;
+        padding: 1.5rem;
+    }
+
     .row_7 {
         background-image: url(../assets/img/Holz_mitSchatten_7.png);
         background-repeat: no-repeat;
@@ -280,28 +304,23 @@
         padding-bottom: 1rem;
     }
 
+    .position1 {
+        margin-left: 13.5rem;
+    }
+
+    .position2 {
+        margin-left: 30rem;
+        margin-top: -2.5rem;
+    }
+
+    .position3 {
+        margin-left: -4.5rem;
+        margin-top: -1.5rem;
+        margin-bottom: -1.5rem;
+    }
+
     .position4 {
         margin-left: 1rem;
-    }
-
-
-    .wood_across {
-        background-image: url(../assets/img/wood_across.png);
-        background-repeat: no-repeat;
-    }
-
-    .wood_up_short {
-        background-image: url(../assets/img/wood_up_short.png);
-        background-repeat: no-repeat;
-        margin-left: 12rem;
-    }
-
-    .wood_up_long {
-        background-image: url(../assets/img/wood_up_long.png);
-        background-repeat: no-repeat;
-        margin-left: 18rem;
-        margin-top: -14rem;
-        padding-bottom: 2rem;
     }
 
     .codi {
@@ -326,19 +345,6 @@
         height: auto;
         right: 1rem;
         bottom: 20rem;
-    }
-
-    .position1 {
-
-    }
-
-    .position2 {
-        margin-left: 13.5rem;
-    }
-
-    .position3 {
-        margin-left: 30rem;
-        margin-top: -2.5rem;
     }
 
 

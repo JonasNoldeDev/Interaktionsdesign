8e157c73e2e455a7994182bbd39515fa2b7648c0
Leonie <leonie.kahn@htwg-konstanz.de>
2022-01-19 08:13:16 +0100

    removed back button

--- codi-lernapp/src/pages/Crossword.vue
+++ codi-lernapp/src/pages/Crossword.vue
@@ -1,15 +1,5 @@
 <template>
     <main-layout :visible="pageStep === 0" :hidden="pageStep !== 0">
-        <header>
-            <v-button href="/lernsektionen">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left"
-                     role="img" xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 256 512">
-                    <path fill="currentColor"
-                          d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
-                </svg>
-            </v-button>
-        </header>
         <Character position="right" :pose="characterPose" ref="character">
             <Bubble type="speech-bubble" :visible="currentStep === 0" v-on:next="currentStep++" next>
                 <p>Oh <b>wow!</b></p>
